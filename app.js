2d29cf7c861907f4de2c918960344e77f80ef176
Cam O'Brien <camobrien343@gmail.com>
2018-03-25 20:31:50 -0500

    uploaded app.js and package.json files

---
+++ app.js
@@ -0,0 +1,21 @@
+
+const express = require("express");
+const Twit = require("twit");
+const app = express();
+const routes = require('./routes/index');
+
+// set view engine
+app.use("/static", express.static("public"));
+app.set("view engine", "pug");
+
+// get the routes
+app.use('/', routes);
+
+
+
+// create server
+app.listen(3000, () => {
+console.log("The application is serving on localhost:3000, press ctrl + c to terminate.")
+});
+
+module.exports = app;
